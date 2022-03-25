e20f0c356ba609113a8ca94d9c05241a3f09d6a4
D. Omer <ahmed@telemeddm.com>
2022-03-25 16:40:23 -0400

    add power button override tggle button

--- OtoDecks/Source/LookAndFeel.cpp
+++ OtoDecks/Source/LookAndFeel.cpp
@@ -60,3 +60,89 @@ void LookAndFeelEx::drawRotarySlider(Graphics& g, int x, int y, int width, int h
 
     g.drawLine(backgroundArc.getBounds().getCentreX(), backgroundArc.getBounds().getCentreY(), thumbPoint.getX(), thumbPoint.getY(), lineW/2);
 }
+
+void LookAndFeelEx::drawToggleButton(juce::Graphics& g, juce::ToggleButton& toggleButton, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown)
+{
+    using namespace juce;
+
+    if( auto* pb = dynamic_cast<PowerButton*>(&toggleButton) )
+    {
+        Path powerButton;
+        
+        auto bounds = toggleButton.getLocalBounds();
+        
+        auto size = jmin(bounds.getWidth(), bounds.getHeight()) - 6;
+        auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
+        
+        float ang = 30.f; //30.f;
+        
+        size -= 6;
+        
+        powerButton.addCentredArc(r.getCentreX(),
+                                  r.getCentreY(),
+                                  size * 0.5f,
+                                  size * 0.5f,
+                                  0.f,
+                                  degreesToRadians(ang),
+                                  degreesToRadians(360.f - ang),
+                                  true);
+        
+        powerButton.startNewSubPath(r.getCentreX(), r.getY());
+        powerButton.lineTo(r.getCentre());
+        
+        PathStrokeType pst(2.f, PathStrokeType::JointStyle::curved);
+        
+        auto color = toggleButton.getToggleState() ? Colours::dimgrey : ColorScheme::getSliderRangeTextColor();
+        
+        g.setColour(color);
+        g.strokePath(powerButton, pst);
+        g.drawEllipse(r, 2);
+    } 
+    else
+    {
+        if (toggleButton.hasKeyboardFocus(true))
+        {
+            g.setColour(toggleButton.findColour(TextEditor::focusedOutlineColourId));
+            g.drawRect(0, 0, toggleButton.getWidth(), toggleButton.getHeight());
+        }
+
+        float fontSize = jmin(15.0f, (float)toggleButton.getHeight() * 0.75f);
+        const float tickWidth = fontSize * 1.1f;
+
+        drawTickBox(g, toggleButton, 4.0f, ((float)toggleButton.getHeight() - tickWidth) * 0.5f,
+            tickWidth, tickWidth,
+            toggleButton.getToggleState(),
+            toggleButton.isEnabled(),
+            shouldDrawButtonAsHighlighted,
+            shouldDrawButtonAsDown);
+
+        g.setColour(toggleButton.findColour(ToggleButton::textColourId));
+        g.setFont(fontSize);
+
+        if (!toggleButton.isEnabled())
+            g.setOpacity(0.5f);
+
+        g.drawFittedText(toggleButton.getButtonText(),
+            toggleButton.getLocalBounds().withTrimmedLeft(roundToInt(tickWidth) + 5)
+            .withTrimmedRight(2),
+            Justification::centredLeft, 10);
+    }
+    /*else
+    {
+        auto bounds = toggleButton.getLocalBounds().reduced(2);
+        
+        auto buttonIsOn = toggleButton.getToggleState();
+        
+        const int cornerSize = 4;
+
+        g.setColour(buttonIsOn ?
+                    toggleButton.findColour(TextButton::ColourIds::buttonOnColourId) :
+                    toggleButton.findColour(TextButton::ColourIds::buttonColourId));
+        g.fillRoundedRectangle(bounds.toFloat(), cornerSize);
+        
+        g.setColour(buttonIsOn ? Colours::white : ColorScheme::getTitleColor());
+        g.drawRoundedRectangle(bounds.toFloat(), cornerSize, 1);
+        g.setColour(buttonIsOn ? Colours::black : ColorScheme::getTitleColor());
+        g.drawFittedText(toggleButton.getName(), bounds, Justification::centred, 1);
+    }*/
+}
